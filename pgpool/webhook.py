7f5b3cfec9ce2001b215b1609391e1419e53f422
ngovil21 <ngovil21@gmail.com>
2018-02-11 01:28:54 -0600

    fix webhook format

--- pgpool/webhook.py
+++ pgpool/webhook.py
@@ -242,8 +242,9 @@ class Filter:
 
     def format_webhook(self, data):
         message = self.webhook.get('format', "")
-        for key in data:
-            message = message.replace('<{}>'.format(key), str(data['d']))
+        for key in message:
+            for repl in data:
+                message = message[key].replace('<{}>'.format(repl), str(data[repl]))
         return message
 
 
